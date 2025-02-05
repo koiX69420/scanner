4a559865c3b05ecfcf1ede82cb06dc0dbae6d6cb
Michael Etschbacher <etsch.michael@gmail.com>
2025-02-05 22:25:09 +0100

    no clue

--- tg/tg.js
+++ tg/tg.js
@@ -1,7 +1,10 @@
 const TelegramBot = require('node-telegram-bot-api');
 
 const bot = new TelegramBot(process.env.TG_BOT_TOKEN, { polling: true });
-
+bot.setMyCommands([
+    { command: "fresh", description: "Get top holder wallet freshness and holdings with /fresh ca" },
+    { command: "funding", description: "Get funding information of who funded whom in top holders with /funding ca" },
+  ]);
 
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
@@ -9,6 +12,14 @@ bot.onText(/\/start/, (msg) => {
     // Message to show available commands with descriptions
     const message = `
     🗣 *Welcome to the Bot!*
+  
+    Here are the commands you can use:
+
+    Get top holder wallet freshness and holdings.
+    /fresh [ca]  
+  
+    Get funding information of who funded whom in top holders.
+    /funding [ca]
       `;
 
     bot.sendMessage(chatId, message, { parse_mode: "Markdown" });
