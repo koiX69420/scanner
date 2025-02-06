00c145de7ee866be3aec0add39fcfb24a46706f6
Michael Etschbacher <etsch.michael@gmail.com>
2025-02-06 20:55:31 +0100

    timezone back

--- bot/scanner.js
+++ bot/scanner.js
@@ -265,7 +265,10 @@ function formatTimestamp(timestamp) {
   return date.toLocaleString("en-US", {
     year: "numeric",
     month: "short",
-    day: "numeric"
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    timeZoneName: "short",
   });
 }
 function formatMarketCap(value) {
